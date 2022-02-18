abb34bead015fa1a6eda39e9e6c868c0fb2206c8
Diego Hernandez <diego.hdez@hotmail.com>
2022-02-18 13:55:05 -0600

    Modulo 17
    sistema de particulas
    shadergraph basico
    sistema de audio basico

--- AtrilleryURP/Assets/_Scripts/Canon.cs
+++ AtrilleryURP/Assets/_Scripts/Canon.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class Canon : MonoBehaviour
 {
+    public static bool Bloqueado;
+
+    public AudioClip clipDisparo;
+    private GameObject SonidoDisparo;
+    private AudioSource SourceDisparo;
+
     [SerializeField] private GameObject BalaPrefab;
+    public GameObject ParticulasDisparo;
     private GameObject puntaCanon;
     private float rotacion;
 
     private void Start()
     {
         puntaCanon = transform.Find("PuntaCanon").gameObject;
+        SonidoDisparo = GameObject.Find("SonidoDisparo");
+        SourceDisparo = SonidoDisparo.GetComponent<AudioSource>();
     }
     // Update is called once per frame
     void Update()
@@ -24,13 +33,21 @@ public class Canon : MonoBehaviour
         if (rotacion > 90) rotacion = 90;
         if (rotacion < 0) rotacion = 0;
 
-        if (Input.GetKeyDown(KeyCode.Space)) 
+        if (Input.GetKeyDown(KeyCode.Space)&&!Bloqueado) 
         {
             GameObject temp = Instantiate(BalaPrefab, puntaCanon.transform.position, transform.rotation);
+            
             Rigidbody tempRB = temp.GetComponent<Rigidbody>();
+            SeguirCamara.objetivo = temp;
             Vector3 direccionDisparo = transform.rotation.eulerAngles;
             direccionDisparo.y = 90 - direccionDisparo.x;
+            Vector3 direccionParticulas = new Vector3(-90 + direccionDisparo.x, 90, 0);
+            GameObject Particulas = Instantiate
+                (ParticulasDisparo, puntaCanon.transform.position, Quaternion.Euler(direccionParticulas), transform);
             tempRB.velocity = direccionDisparo.normalized * AdministradorJuego.VelocidadBala;
+            //SourceDisparo.PlayOneShot(clipDisparo);
+            SourceDisparo.Play();
+            Bloqueado = true;
 
         }
     }
